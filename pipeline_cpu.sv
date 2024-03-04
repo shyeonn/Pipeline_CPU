2b38a82f8773450f4d2f237bd3c45bdc97845793
shyeonn <tkdgus2916@ajou.ac.kr>
2024-03-04 21:45:34 +0900

    Implement decode stage and pipeline register except MEM/WB

--- pipeline_cpu.sv
+++ pipeline_cpu.sv
@@ -84,7 +84,7 @@ module pipeline_cpu
     logic   [31:0]  pc_next_plus4, pc_next_branch;
     logic           pc_next_sel;
     logic           branch_taken;
-    //logic           regfile_zero;   // zero detection from regfile, REMOVED
+    logic           regfile_zero;   // zero detection from regfile, REMOVED
 
     assign pc_next_plus4 = pc_curr + 4;
     assign pc_next_sel = /* FILL THIS */ 
@@ -94,6 +94,7 @@ module pipeline_cpu
         if (~reset_b) begin
             pc_curr <= 'b0;
         end else begin
+            pc_curr = pc_next;
              /* FILL THIS */ 
         end
     end
@@ -129,11 +130,11 @@ module pipeline_cpu
         if (~reset_b) begin
             id <= 'b0;
         end else begin
-            if (  /* FILL THIS */ ) begin
+            if (if_flush) begin
                 id <= 'b0;
-            end else if (  /* FILL THIS */ ) begin
-                id.pc <=  /* FILL THIS */ 
-                id.inst <=  /* FILL THIS */ 
+            end else if (~if_stall) begin
+                id.pc <= pc_curr;  
+                id.inst <=  inst;
             end
         end
     end
@@ -161,28 +162,21 @@ module pipeline_cpu
     logic   [6:0]   funct7;
     logic   [2:0]   funct3;
 
-    // COMPLETE THE MAIN CONTROL UNIT HERE
+    assign opcode = id.inst[6:0];
+    assign branch[0] = ((opcode==7'b1100011) && (funct3==3'b000)) ? 1'b1: 1'b0;
+    assign branch[1] = ((opcode==7'b1100011) && (funct3==3'b001)) ? 1'b1: 1'b0;
+    assign branch[2] = ((opcode==7'b1100011) && (funct3==3'b100)) ? 1'b1: 1'b0;
+    assign branch[3] = ((opcode==7'b1100011) && (funct3==3'b101)) ? 1'b1: 1'b0;
 
+    assign mem_read = (opcode==7'b0000011) ? 1'b1: 1'b0;    // ld
+    assign mem_write = (opcode==7'b0100011) ? 1'b1: 1'b0;   // sd
+    assign mem_to_reg = mem_read;
+    assign reg_write = (opcode==7'b0110011) | (opcode==7'b0010011) | mem_read; // ld, r-type, or i-type
+    // ld, sd, or i-type
+    assign alu_src = ( mem_read | mem_write | (opcode==7'b0010011) ) ? 1'b1: 1'b0;   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    assign alu_op[0] = |branch;
+    assign alu_op[1] = (opcode==7'b0110011) | (opcode==7'b0010011);    // r-type or i-type
 
 
     // --------------------------------------------------------------------
@@ -197,12 +191,13 @@ module pipeline_cpu
     // COMPLETE IMMEDIATE GENERATOR HERE
     logic   [11:0]  imm12;
 
-    assign imm12 =  /* FILL THIS */ 
-    assign imm32 =  /* FILL THIS */ 
-    assign imm32_branch =  /* FILL THIS */ // << 1 for branch
+    assign imm12 = (|branch) ? {inst[31], inst[7], inst[30:25], inst[11:8]}: 
+                ( (mem_write) ? {inst[31:25], inst[11:7]}: inst[31:20] );
+    assign imm32 = { {20{imm12[11]}}, imm12 };
+    assign imm32_branch = {imm32[30:0], 1'b0}; // << 1 for branch
 
     // Computing branch target
-    assign pc_next_branch =  /* FILL THIS */ 
+    assign pc_next_branch =  pc_curr + imm32_branch;
 
     // ----------------------------------------------------------------------
 
@@ -219,15 +214,15 @@ module pipeline_cpu
     logic           id_stall, id_flush;
 
 
-    assign stall_by_load_use =  /* FILL THIS */ 
-    assign flush_by_branch =  /* FILL THIS */ 
+    assign stall_by_load_use = ex.mem_read && ((ex.rd == rs1) || (ex.rd == rs2));
+    assign flush_by_branch = branch_taken; 
   
-    assign id_flush =  /* FILL THIS */ 
-    assign id_stall =  /* FILL THIS */ 
+    assign id_flush =  flush_by_branch
+    assign id_stall =  stall_by_load_use;
 	
-    assign if_flush =  /* FILL THIS */ 
+    assign if_flush =  flush_by_branch;
     assign if_stall =  /* FILL THIS */ 
-    assign pc_write =  /* FILL THIS */ 
+    assign pc_write =  /* FILL THIS */
 
     // ----------------------------------------------------------------------
 
@@ -237,26 +232,31 @@ module pipeline_cpu
     logic   [REG_WIDTH-1:0] rd_din;
     logic   [REG_WIDTH-1:0] rs1_dout, rs2_dout;
     
-    assign rs1 =  /* FILL THIS */     // our processor does NOT support U and UJ types
-    assign rs2 =  /* FILL THIS */     // consider ld and i-type
-    assign rd =  /* FILL THIS */ 
+    assign rs1 = inst[19:15];
+    assign rs2 = inst[24:20];
+    assign rd = inst[11:7];
     // rd, rd_din, and reg_write will be determined in WB stage
     
     // instnatiation of register file
     regfile #(
         .REG_WIDTH          (REG_WIDTH)
     ) u_regfile_0 (
-        .clk                ( /* FILL THIS */ ),
-        .rs1                ( /* FILL THIS */ ),
-        .rs2                ( /* FILL THIS */ ),
-        .rd                 ( /* FILL THIS */ ),
-        .rd_din             ( /* FILL THIS */ ),
-        .reg_write          ( /* FILL THIS */ ),
-        .rs1_dout           ( /* FILL THIS */ ),
-        .rs2_dout           ( /* FILL THIS */ )
+        //From ID stage
+        .clk                (clk),
+        .rs1                (rs1),
+        .rs2                (rs2),
+        .rd                 (rd),
+        //From WB stage
+        .rd_din             (rd_din),
+        .reg_write          (reg_write),
+        .rs1_dout           (rs1_dout),
+        .rs2_dout           (rs2_dout)
     );
 
-    //assign regfile_zero = ~|(rs1_dout ^ rs2_dout); // REMOVED
+    assign regfile_zero = ~|(rs1_dout ^ rs2_dout);
+
+    assign funct7 = inst[31:25];
+    assign funct3 = inst[14:12];
 
     // ------------------------------------------------------------------
 
@@ -271,23 +271,21 @@ module pipeline_cpu
             ex <= 'b0;
         end else begin
              /* FILL THIS */ 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
+             ex.pc <= id.pc;
+             ex.rs1_dout <= rs1_dout;
+             ex.rs2_dout <= rs2_dout;
+             ex.imm32 <= imm32;
+             ex.funct3 <= funct3;
+             ex.funct7 <= funct7;
+             ex.branch <= branch;
+             ex.alu_src <= alu_src;
+             ex.alu_op <= alu_op;
+             ex.mem_read <= mem_read;
+             ex.mem_write <= mem_write;
+             ex.rs1 <= rs1;
+             ex.rs2 <= rs2;
+             ex.reg_write <= reg_write;
+             ex.mem_to_reg <= mem_to_reg;
         end
     end
 
@@ -298,6 +296,11 @@ module pipeline_cpu
      * - ALU & ALU control
      * - Data forwarding unit
      */
+     alu
+
+
+
+     
 
     // --------------------------------------------------------------------
     /* ALU control unit:
@@ -308,19 +311,22 @@ module pipeline_cpu
     logic   [3:0]   alu_control;    // ALU control signal
 
     // COMPLETE ALU CONTROL UNIT
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    //
+
+    // COMPLETE THE ALU CONTROL UNIT HERE
+
+    always_comb begin
+        if (alu_op[1]) begin
+            case (ex.funct3)
+                3'b111: alu_control = 4'b0000;
+                3'b110: alu_control = 4'b0001;
+                3'b100: alu_control = 4'b0011;
+                default: alu_control = (funct7[5]) ? 4'b0110: 4'b0010;
+            endcase
+        end else begin
+            alu_control = (alu_op[0]) ? 4'b0110: 4'b0010;
+        end
+    end
 
     // ---------------------------------------------------------------------
 
@@ -333,6 +339,7 @@ module pipeline_cpu
 
 	/* verilator lint_off CASEX */
    // COMPLETE FORWARDING MUXES
+
    
    
    
@@ -400,7 +407,7 @@ module pipeline_cpu
     assign sub_for_branch =  /* FILL THIS */ 
     assign bu_zero =  /* FILL THIS */ 
     assign bu_sign =  /* FILL THIS */ 
-    assign branch_taken =  /* FILL THIS */ 
+    assign branch_taken =  /* FILL THIS */
 
     // -------------------------------------------------------------------------
     /* Ex/MEM pipeline register
@@ -411,14 +418,13 @@ module pipeline_cpu
         if (~reset_b) begin
             mem <= 'b0;
         end else begin
-			/* FILL THIS */ 
-	  
-	  
-	  
-	  
-	  
-	  
-	  
+            mem.alu_result <= alu_result;
+            mem.rs2_dout <= ex.rs2_dout;
+            mem.mem_read <= ex.mem_read;
+            mem.mem_write <= ex.mem_write;
+            mem.rd <= ex.rd;
+            mem.reg_write <= ex.reg_write;
+            mem.mem_to_reg <= ex.mem_to_reg;
         end
     end
 
