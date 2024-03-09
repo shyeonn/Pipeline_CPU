692ed68223daab1d5a7579003323275b773763c5
shyeonn <tkdgus2916@ajou.ac.kr>
2024-03-10 03:52:44 +0900

    Expand alu instruction

--- alu.sv
+++ alu.sv
@@ -14,8 +14,8 @@
 module alu
 #(  parameter REG_WIDTH = 32 )  // ALU input data width is equal to the width of register file
 (
-    input   [REG_WIDTH-1:0] in1,    // Operand 1
-    input   [REG_WIDTH-1:0] in2,    // Operand 2
+    input signed  [REG_WIDTH-1:0] in1,    // Operand 1
+    input signed  [REG_WIDTH-1:0] in2,    // Operand 2
     input   [3:0]   alu_control,    // ALU control signal
     output  logic [REG_WIDTH-1:0] result // ALU output
     //output          zero,           // Zero detection
@@ -29,6 +29,9 @@ module alu
             //4'b0010: result = in1 + in2;
 			4'b0011: result = in1 ^ in2;
             4'b0110: result = in1 - in2;
+            4'b1000: result = in1 << in2;
+            4'b1010: result = in1 >> in2;
+            4'b1011: result = in1 >>> in2;
             default: result = in1 + in2;    // default = add
 		endcase
     end
